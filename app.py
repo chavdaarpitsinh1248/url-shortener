df235ebe1087ed70eb8ecca5a0b0cf7aeadae729
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2026-02-05 17:22:26 +0530

    Add URL shortening endpoint with database persistence

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models import create_tables
+from database import insert_url, save_short_code
+from shortener import encode_base62
+
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "URL Shortener is Running"
+
+
+@app.route("/shorten", methods=["POST"])
+def shorten_url():
+    data = request.get_json()
+    
+    if not data or "url" not in data:
+        return jsonify({"error": "URL is required"}), 400
+    
+    original_url = data["url"]
+    
+    # Step 1: Insert URL
+    url_id = insert_url(original_url)
+    
+    # Step 2: Generate short code
+    short_code = encode_base62(url_id)
+    
+    # Step 3: Save short code
+    save_short_code(url_id, short_code)
+    
+    short_url = request.host_url + short_code
+    
+    return jsonify({"short_url": short_url}), 201
 
 if __name__ == "__main__":
     create_tables()
--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 import sqlite3
+from sqlite3 import IntegrityError
+
 
 DB_PATH = "data/urls.db"
 
@@ -6,3 +8,32 @@ def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
+
+
+def insert_url(original_url):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute(
+        "INSERT INTO urls (original_url) VALUES (?)",
+        (original_url,)
+    )
+    
+    conn.commit()
+    url_id = cursor.lastrowid
+    conn.close()
+    
+    return url_id
+
+
+def save_short_code(url_id, short_code):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute(
+        "UPDATE urls SET short_code = ? WHERE id = ?",
+        (short_code, url_id)
+    )
+    
+    conn.commit()
+    conn.close()
